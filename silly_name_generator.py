7ca5a00b5852fdcf6e52f819cf00ec849ab41a10
Aman Pandit <amanpandit3027@gmail.com>
2023-01-28 09:09:20 +0530

    Create silly_name_generator.py

---
+++ silly_name_generator.py
@@ -0,0 +1,12 @@
+# Silly Name Generator.
+
+import random
+
+print('Welcome to Silly Name Generator')
+start = input('Press Enter to start: ')
+name = [
+    'break', 'hole-cat', 'dabar', 'fulfy', 'Marijuana Pepsi Jackson', 'Tokyo Sexwale', 'big dick bee', 'ligma ball', 'dumb bitchA', 'can somebody give a hoooooo ya', 'anna borshun', 'mike oxlong', 'tess tickle', 'mike oochie', 'hugh jass', 'Ben dover', 'gabe itch', 'mike hunt', 'ore fin', 'emma nut', 'nut meg', 'sucker cold', 'women at wrong side', 'glen', 'blue tit', 'swallo', 'woodpeaker', 'nuthatch', 'boopie', 'bozo', 'side squirt', 'my bitch is gay', 'chad', 'champ', 'daddy oatmeal', 'average_student', 'hoosier_daddy', 'cute.as.ducks', 'real_name_is_headen', 'yellow snowman', 'op__rah', 'joe_mama', 'everybody', 'pawneeGoddess', 'Iyellalot', 'banana_round_clock', 'pnur', 'fartooolong', 'UFO_ Hohohoo', 'Omni_cor', 'heks_splits', 'Brad_lite', 'bikny_bottom', 'papa_ki_pari', 'aareee_didi', 'gadi', 'kushbu', 'badbu', 'groout', 'Auf Wiedersehen', 'Tschüss', ' Morgen', 'Abend', 'ja', 'vielleicht', 'Privyet', 'ni hao', 'bud_darkyears', 'in_jail_out_soon', 'kiss_my_axe', 'King of diary queen', 'poor_rambo', 'sofa king cool', 'fur real', 'diss_bish', 'in'
+    'BIMBO', 'biGMEAT', 'Bloominshine', 'clovenhoof', 'clutterbuck', 'lakadbagha', 'cocktoasten', 'hootkins', 'king_peasent', 'noseworthy', "M'bembo", 'oxhandler', 'peaklike', 'pennywhistle', 'pieplow', 'Quakenbush', 'splerm', 'wollysocks', 'kawaii', 'sugoi', 'baka', 'tomodachi', "can't", 'nani', 'Jon Snow', 'Cersei Lannister', 'Quent', 'Quaithe', 'Qotho', 'Mance Rayder', 'Qhorin Halfhand', 'Steelshanks Walton', 'Hizdahr zo Loraq', 'Leaf', 'Aeron Greyjoy', 'Alfie Solomons', 'Linda', 'Gina', 'Darby', 'Sabini', 'Chester', 'Luca', 'Campbell', 'tommy', 'Delsin', ' Changretta', 'Rowe', ' Edna', 'Randle', 'Mode', 'McMurphy', 'ptimus Prime', 'Norman', ' Bates', 'Minions', 'Maximus', 'Legolas', 'Wednesday', 'dollar', 'Addams', 'tree', 'Clouseau', 'Inigo', 'Inspector', ' Hal', 'Gromit', 'Ethan Hunt', ' Red', 'Walker', 'Corporal chicks', 'Bane', 'Woody', 'take a walk', 'captain underpants', 'Withnail', 'Batty', 'Martin', 'Samwise', 'Blank', 'Private', 'Salander', 'Frank', 'Hudson', 'Donnie', 'Darko', 'Tony Montana', 'Gunderson', 'Sméagol', 'Landa', 'Landa', 'loda', 'Bilbo', 'King'
+
+]
+print(random.choice(name))
